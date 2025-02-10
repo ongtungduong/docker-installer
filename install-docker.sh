460f716ac4e639dad5b05ec468a5d41caa896b57
Ông Tùng Dương <ongtungduong@gmail.com>
2025-02-10 20:59:54 +0700

    feature: install docker with apt repo for ubuntu, debian, raspbian

--- install-docker.sh
+++ install-docker.sh
@@ -5,7 +5,9 @@ function getOS(){
     echo "${ID}"
 }
 
-function installDockerUbuntu() {
+function installDockerAPT() {
+    OS=$(getOS)
+    
     # Update package list and install necessary dependencies
     sudo apt-get update -y
     sudo apt-get install ca-certificates curl -y
@@ -14,36 +16,13 @@ function installDockerUbuntu() {
     sudo install -m 0755 -d /etc/apt/keyrings
 
     # Add Docker's GPG key to the keyring
-    sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+    sudo curl -fsSL "https://download.docker.com/linux/${OS}/gpg" -o /etc/apt/keyrings/docker.asc
     sudo chmod a+r /etc/apt/keyrings/docker.asc
 
     # Add Docker repository to APT sources
     echo \
-    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/${OS} \
     $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
-    sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-
-    # Update package list again to include Docker repository
-    sudo apt-get update -y
-
-    # Install Docker Engine, Docker CLI, containerd.io, docker-buildx-plugin, and docker-compose-plugin
-    sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
-}
-
-function installDockerDebian() {
-    # Update package list and install necessary dependencies
-    sudo apt-get update -y
-    sudo apt-get install ca-certificates curl -y
-
-    # Create the keyrings directory if it doesn't exist
-    sudo install -m 0755 -d /etc/apt/keyrings
-    sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
-    sudo chmod a+r /etc/apt/keyrings/docker.asc
-
-    # Add the repository to Apt sources:
-    echo \
-    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
-    $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
     sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
     # Update package list again to include Docker repository
@@ -76,11 +55,8 @@ function main() {
     checkDockerInstallation
 
     case $(getOS) in
-        "ubuntu")
-            installDockerUbuntu
-            ;;
-        "debian")
-            installDockerDebian
+        "ubuntu"|"debian"|"raspbian")
+            installDockerAPT
             ;;
         *)
             echo "Your operating system is not supported."
