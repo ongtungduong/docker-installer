7e947dc7f30bb13a2abb80acb0bf95f4dd3be03a
Ông Tùng Dương <ongtungduong@gmail.com>
2025-02-10 20:51:12 +0700

    feature: install Docker CE for Debian

--- install-docker.sh
+++ install-docker.sh
@@ -1,33 +1,56 @@
 #!/bin/bash
 
-function checkOS() {
+function getOS(){
     source /etc/os-release
-    if [[ ${ID} == "ubuntu" ]] && [[ $(echo "${VERSION_ID}" | cut -d'.' -f1) -ge 20 ]]; then
-        echo "ubuntu"
-    fi
+    echo "${ID}"
 }
 
 function installDockerUbuntu() {
     # Update package list and install necessary dependencies
     sudo apt-get update -y
-    sudo apt-get install ca-certificates curl gnupg -y
+    sudo apt-get install ca-certificates curl -y
 
     # Create the keyrings directory if it doesn't exist
     sudo install -m 0755 -d /etc/apt/keyrings
 
     # Add Docker's GPG key to the keyring
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-    sudo chmod a+r /etc/apt/keyrings/docker.gpg
+    sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+    sudo chmod a+r /etc/apt/keyrings/docker.asc
 
     # Add Docker repository to APT sources
-    echo "deb [arch=\"$(dpkg --print-architecture)\" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    echo \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+    $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+    sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
     # Update package list again to include Docker repository
     sudo apt-get update -y
 
     # Install Docker Engine, Docker CLI, containerd.io, docker-buildx-plugin, and docker-compose-plugin
     sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+}
 
+function installDockerDebian() {
+    # Update package list and install necessary dependencies
+    sudo apt-get update -y
+    sudo apt-get install ca-certificates curl -y
+
+    # Create the keyrings directory if it doesn't exist
+    sudo install -m 0755 -d /etc/apt/keyrings
+    sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+    sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+    # Add the repository to Apt sources:
+    echo \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+    $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+    sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+    # Update package list again to include Docker repository
+    sudo apt-get update -y
+
+    # Install Docker Engine, Docker CLI, containerd.io, docker-buildx-plugin, and docker-compose-plugin
+    sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 }
 
 function checkDockerInstallation() {
@@ -40,7 +63,7 @@ function checkDockerInstallation() {
 
 function manageDockerAsNonRootUser() {
     sudo groupadd docker
-	sudo usermod -aG docker $USER
+	sudo usermod -aG docker $n
 	newgrp docker
 }
 
@@ -51,9 +74,13 @@ function configureDockerToStartOnBoot() {
 
 function main() {
     checkDockerInstallation
-    case $(checkOS) in
+
+    case $(getOS) in
         "ubuntu")
             installDockerUbuntu
+            ;;
+        "debian")
+            installDockerDebian
             ;;
         *)
             echo "Your operating system is not supported."
